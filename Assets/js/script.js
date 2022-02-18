0bb7666b99c89111e4a1afec50bb83c559849ee4
Andreas Kaiafas <akaiafas526@gmail.com>
2022-02-18 00:07:59 -0500

    update 2 to js

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -2,24 +2,23 @@
 let generateBtn = document.querySelector("#generate");
 
 //  GLobal Variables
-let enter
-let uppercaseConfirm
-let numberConfirm
-let characterConfirm
 
 // character Values
-let character = ('!', '&', '$', '#', '@', '*','+','?')
+let special = ('!', '&', '$', '#', '@', '*','+','?');
 // Number Values
-let number = ('1','2','3','4','5','6','7','8','9','0')
-// 
-let charupperCase = ('A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z')
-// 
-let charlowerCase = ('a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z')
+let number = ('1','2','3','4','5','6','7','8','9','0');
+// Uppercase Values
+let upperCase = ('A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z');
+// Lowercase Values
+let lowerCase = ('a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z');
+// Chosen character Values
+let chosencharacter = ('');
 
 function generatePassword() {
-  let passwordSTR;
+  let passwordStr = ('');
+  let passwordlength = prompt('Choose a length for your password between 8 & 128');
   
-  return password;
+  return passwordStr;
 }
 
 // Write password to the #password input
