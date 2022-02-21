c4366c66ba50d9189d0f15f6be2b6184ee9a1c05
Andreas Kaiafas <akaiafas526@gmail.com>
2022-02-20 22:55:37 -0500

    added window prompts

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,25 +1,50 @@
 // Assignment Code
 let generateBtn = document.querySelector("#generate");
 
+let passwordText = document.querySelector("#password");
+
 //  GLobal Variables
 
-// character Values
-let special = ('!', '&', '$', '#', '@', '*','+','?');
+// Character Values
+let special = ['!', '&', '$', '#', '@', '*', '+', '?'];
 // Number Values
-let number = ('1','2','3','4','5','6','7','8','9','0');
+let number = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0'];
 // Uppercase Values
-let upperCase = ('A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z');
+let upperCase = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
 // Lowercase Values
-let lowerCase = ('a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z');
+let lowerCase = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
 // Chosen character Values
-let chosencharacter = ('');
+let finalChosenCharacter = [];
+// let array = new Array
+
+let passwordLength = prompt('Choose a length for your password between 8 & 128 characters.');
+
+while (passwordLength > 128 || passwordLength < 8) {
+  alert('Password should be greater than 8 characters and less than 128.')
+  passwordLength = prompt('Choose a length for your password between 8 & 128 characters.');
+}
+
+// confirm('Do you want special characters?');
+// confirm('Do you want numbers?');
+// confirm('Do you want uppercase?');
+// confirm('Do you want lowercase?');
+
+let specialBoolean = confirm('Do you want special characters?');
+let numberBoolean = confirm('Do you want numbers?');
+let uppercaseBoolean = confirm('Do you want uppercase?');
+let lowercaseBoolean = confirm('Do you want lowercase?');
+
+
 
 function generatePassword() {
-  let passwordStr = ('');
-  let passwordlength = prompt('Choose a length for your password between 8 & 128');
-  
+  let passwordStr = '';
+  console.log(passwordStr)
+
+
+
   return passwordStr;
 }
+
 
 // Write password to the #password input
 function writePassword() {
@@ -32,3 +57,5 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+generateBtn.addEventListener("click", generatePassword);
+
